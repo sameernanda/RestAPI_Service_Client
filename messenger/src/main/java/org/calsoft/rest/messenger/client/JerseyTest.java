b1fb1b0f7f00ea3d10d1067accbcb5998b43a902
Samira Nanda <samira.nanda@calsoftinc.com>
2017-02-27 17:42:38 +0530

    Modified and added new Methods

--- messenger/src/main/java/org/calsoft/rest/messenger/client/JerseyTest.java
+++ messenger/src/main/java/org/calsoft/rest/messenger/client/JerseyTest.java
@@ -1,29 +1,66 @@
 package org.calsoft.rest.messenger.client;
-
+import javax.ws.rs.core.MediaType;
+import org.calsoft.rest.messenger.model.Message;
+import org.codehaus.jackson.map.ObjectMapper;
 import com.sun.jersey.api.client.Client;
 import com.sun.jersey.api.client.ClientResponse;
 import com.sun.jersey.api.client.WebResource;
-
 public class JerseyTest {
+	String url = "http://localhost:8080/messenger/webapi/messages";
+	Client client = Client.create();
 
 	static {
 		DisableSSLVerification.disable();
 	}
 
-	public static void main(String[] args) {
+	public void  getMessage(){
 		try {
-
-			Client client = Client.create();
-			WebResource webResource = client.resource("http://localhost:8080/messenger/webapi/messages");
+			WebResource webResource = client.resource(url);
 			ClientResponse response = webResource.accept("application/json").get(ClientResponse.class);
-			if (response.getStatus() != 200) {
-				throw new RuntimeException("Failed : HTTP error code : "+ response.getStatus());
-			}
-			String output = response.getEntity(String.class);
-			System.out.println("Output from Server .... \n");
-			System.out.println(output);
+			System.out.println(response.getEntity(String.class));
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
+	}
+	
+	public void  createMessage(){
+		try {
+			WebResource webResource = client.resource(url);
+			Message msg = new Message();
+			msg.setAuthor("Sag");
+			msg.setMessage("Sagar Here");
+			ObjectMapper mapper = new ObjectMapper();
+			String requestEntity = mapper.writeValueAsString(msg);
+			ClientResponse response = webResource.accept(MediaType.APPLICATION_JSON).header("Content-type", MediaType.APPLICATION_JSON).post(ClientResponse.class,requestEntity);
+			System.out.println(response.getEntity(String.class));
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+	
+	public void  updateMessage(String id){
+		try {
+			WebResource webResource = client.resource(url).path(id);
+			Message msg = new Message();
+			msg.setAuthor("Sag");
+			msg.setMessage("Sagar Kumar Nanda");
+			ObjectMapper mapper = new ObjectMapper();
+			String requestEntity = mapper.writeValueAsString(msg);
+			ClientResponse response = webResource.accept(MediaType.APPLICATION_JSON).header("Content-type", MediaType.APPLICATION_JSON).put(ClientResponse.class,requestEntity);
+			System.out.println(response.getEntity(String.class));
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+	public static void main(String[] args) {
+		JerseyTest jTest = new JerseyTest();
+			jTest.getMessage();
+			System.out.println("######################################################################################################################");
+			jTest.createMessage();
+			System.out.println("######################################################################################################################");
+			jTest.updateMessage("3");
+			System.out.println("######################################################################################################################");
+			jTest.getMessage();
 	}
 }
--- messenger/src/main/java/org/calsoft/rest/messenger/client/RestTemplateTest.java
+++ messenger/src/main/java/org/calsoft/rest/messenger/client/RestTemplateTest.java
@@ -1,28 +1,76 @@
 package org.calsoft.rest.messenger.client;
-
 import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
 
+import org.calsoft.rest.messenger.model.Message;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.client.RestTemplate;
-
 public class RestTemplateTest {
 
+	String url = "http://localhost:8080/messenger/webapi/messages";
+	RestTemplate restTemplate = new RestTemplate();
+	HttpHeaders headers = new HttpHeaders();
 	static {
 		DisableSSLVerification.disable();
 	}
+	
+	public void  getMessage(){
+		try {
+			headers.setAccept(Arrays.asList(MediaType.APPLICATION_JSON));
+			HttpEntity<String> entity = new HttpEntity<String>(headers);
+			ResponseEntity<String> response = restTemplate.exchange(url,HttpMethod.GET, entity, String.class);
+			System.out.println(response.getBody());
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+	public void  createMessage(){
+		try {
+			headers.setContentType(MediaType.APPLICATION_JSON);
+			Message msg = new Message();
+			msg.setAuthor("Sag");
+			msg.setMessage("Sagar Here");
+			HttpEntity<Message> entity = new HttpEntity<Message>(msg,headers);
+			ResponseEntity<String> response = restTemplate.exchange(url,HttpMethod.POST, entity, String.class);
+			System.out.println(response.getBody());
+		} catch (Exception e) {
+			System.out.println(e.getMessage());
+			e.printStackTrace();
+		}
+	}
+
+	public void  updateMessage(){
+		try {
+			String urlForPut = "http://localhost:8080/messenger/webapi/messages/{id}";
+			headers.setContentType(MediaType.APPLICATION_JSON);
+			Map<String,String> param = new HashMap<String,String>();
+			param.put("id","3");
+			Message msg = new Message();
+			msg.setAuthor("Sag");
+			msg.setMessage("Sagar Kumar Nanda");
+			HttpEntity<Message> entity = new HttpEntity<Message>(msg,headers);
+			ResponseEntity<String> response = restTemplate.exchange(urlForPut,HttpMethod.PUT, entity, String.class,param);
+			System.out.println(response.getBody());
+		} catch (Exception e) {
+			System.out.println(e.getMessage());
+			e.printStackTrace();
+		}
+	}
 
 	public static void main(String args[]) {
-		RestTemplate restTemplate = new RestTemplate();
-		HttpHeaders headers = new HttpHeaders();
-
-		headers.setAccept(Arrays.asList(MediaType.APPLICATION_JSON));
-		HttpEntity<String> entity = new HttpEntity<String>(headers);
-		String url = "http://localhost:8080/messenger/webapi/messages";
-		ResponseEntity<String> response = restTemplate.exchange(url,HttpMethod.GET, entity, String.class);
-		System.out.println(response.getBody());
+		RestTemplateTest rTest = new RestTemplateTest();
+		rTest.getMessage();
+		System.out.println("######################################################################################################################");
+		rTest.createMessage();
+		System.out.println("######################################################################################################################");
+		rTest.updateMessage();
+		System.out.println("######################################################################################################################");
+		rTest.getMessage();
 	}
 }
